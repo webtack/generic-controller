a3b030883d9d91b506cfbe1c2e4bef596ff986b5
Ruslan Bilyshko <ruslan.bilyshko@gmail.com>
2018-02-25 00:18:57 +0200

    Template handler

--- src/Traits/TemplateHandlerTrait.php
+++ src/Traits/TemplateHandlerTrait.php
@@ -1,13 +1,43 @@
 <?php namespace Webtack\GenericController\Traits;
 
 
-trait TemplateTrait {
+trait TemplateHandlerTrait {
 	
 	public $templateName = '';
 	
-	
 	private function getTemplateName() {
-		return "test";
+		
+		if($this->templateName === '') {
+			$className = $this->getClassName();
+			$className = $this->replaceTemplateName($className);
+			return $className;
+		}
+		else {
+			return $this->templateName;
+		}		
 	}
 	
+	public function renderToResponse($context) {		
+		$templateName = $this->getTemplateName();		
+		return view($templateName, $context);
+	}
+	
+	private function getClassName() {
+		$path = get_class($this);
+		$path = explode("\\", $path);
+		$path = array_pop($path);	
+		
+		return $path;
+	}
+	
+	private function replaceTemplateName(string $className) {
+		$className = str_replace("Controller", "", $className);
+		$className = preg_replace("/(?=[A-Z])/", "-", $className);
+		$className = str_replace("_", "-", $className);
+		$className = str_replace("--", "-", $className);
+		$className = mb_strtolower($className);
+		$className = substr($className, 1);
+		
+		return $className;
+	}
 }
