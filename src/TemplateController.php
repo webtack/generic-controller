7e3cf58a5ab202d6dac1c7f9fa9e4c45e9eec440
Ruslan Bilyshko <ruslan.bilyshko@gmail.com>
2018-02-25 23:47:45 +0200

    Fix logical in Template Controller

--- src/TemplateController.php
+++ src/TemplateController.php
@@ -3,10 +3,31 @@
 use Webtack\GenericController\Traits\ContextHandlerTrait;
 use Webtack\GenericController\Traits\TemplateHandlerTrait;
 
-class GenericTemplateController extends GenericBaseController {
+/**
+ * Class TemplateController
+ * Processes the specified template using a context that contains parameters from the URL.
+ * 
+ * @package Webtack\GenericController
+ */
+abstract class TemplateController extends BaseController {
 	
 	use ContextHandlerTrait;
 	use TemplateHandlerTrait;
+	
+	protected function initProperties() {
+		$this->templateName = $this->templateName();
+		$this->templatePrefix = $this->templatePrefix();
+		$this->templateSuffix = $this->templateSuffix();
+	}
+	
+	
+	/**
+	 * Init templateName from view
+	 *
+	 * @return string
+	 */
+	abstract protected function templateName();
+	
 	
 	public function get() {
 		$context = $this->getContextData($this->request);
